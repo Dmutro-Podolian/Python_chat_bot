b33619fe5eeb7be0e191b389e21443655bb0a992
Dmutro-Podolian <118061088+Dmutro-Podolian@users.noreply.github.com>
2023-02-19 17:11:22 +0200

    program-changed

--- chat_bot.py
+++ chat_bot.py
@@ -1,5 +1,27 @@
-print("Як тебе звати?")
-def my_function(name):
-    print("Привіт, "+name+"! Коли ти народився?")
-my_function = input() 
-birthday = input()
+def main():
+    name = get_name()
+    print(f"Привіт, {name}")
+
+
+def get_name():
+    print("Введіть ім'я")
+    name = input()
+    if name == "":
+        print("Ведено порожнє значення")
+        get_name()
+    if has_digit(name):
+        print("Цифри недопустимі в імені")
+        get_name()
+
+    return name
+
+
+def has_digit(value):
+    for char in value:
+        if char.isdigit():
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    main()
